e3b00c10f0a339ef7d6bb6f2a1e6778093e4c3bf
App Academy Student <student@appacademy.io>
2016-09-16 16:06:30 -0700

    super computer player complete

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,14 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    current_node = TicTacToeNode.new(game.board, mark)
+    child_nodes = current_node.children
+    winners = child_nodes.select { |node| node.winning_node?(mark) }
+    winner = winners.first.prev_move_pos if !winners.first.nil?
+    return winner if winner
+    not_losers = child_nodes.reject { |node| node.losing_node?(mark) }
+    return not_losers.first.prev_move_pos if not_losers.first
+    raise "I refuse to lose"
   end
 end
 
